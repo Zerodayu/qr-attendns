2f9224ae4b171b6fdc58c16905cc1e8f3ddd4aa0
Zerodayu <kimianalconera39@gmail.com>
2026-06-20 20:00:08 +0800

    update: shadcn update to latest and uses base ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark"> 
+    <html lang="en" className={cn("dark", "font-sans", inter.variable)}> 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
