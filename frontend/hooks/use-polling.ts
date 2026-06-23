b3d66c4c3a31fa9cbed39836c67df6bee436d46e
Zerodayu <kimianalconera39@gmail.com>
2026-06-23 17:19:16 +0800

    new: added data pulling and qr reader hook

---
+++ frontend/hooks/use-polling.ts
@@ -0,0 +1,9 @@
+import { useEffect } from "react"
+
+export function usePolling(callback: () => void, intervalMs: number) {
+  useEffect(() => {
+    if (intervalMs <= 0) return
+    const id = setInterval(callback, intervalMs)
+    return () => clearInterval(id)
+  }, [callback, intervalMs])
+}
